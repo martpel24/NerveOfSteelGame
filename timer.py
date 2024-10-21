0fc28bad917fabdca375c2b8ba34bec71c96954c
martpel24 <martinpellikka@gmail.com>
2024-10-21 18:12:03 -0400

    added random timer betwen 10 - 25

--- timer.py
+++ timer.py
@@ -9,27 +9,22 @@ timer.py uses the time library to help keep track of time
 # This program is timer that counts down
 
 ## Random library
-
-## sample between 10 - 25 - range(10,26)
-
-## Times Up 
-    ## Display image
-
-## Write names of the people who sat down
-## when one name remain state winner 
-
-
+import random
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
 from PIL import Image # the pillow library makes it easy to display images 
 
 im = Image.open("times-up.jpeg")
 
-# ask user to enter desired countdown time
-set_time = int(input("Please set your timer in seconds: "))
-
 print("Players, stand up")
-time.sleep(random.randint(5,25))
+## sample between 10 - 25 - range(10,26)
+time.sleep(random.randint(10,25))
+
+## Times Up 
+    ## Display image
 print("Time's up! Last to sit down wins.")
 
 im.show()
 
+
+## Write names of the people who sat down
+## when one name remain state winner 
